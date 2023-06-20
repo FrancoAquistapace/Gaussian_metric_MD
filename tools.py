a78565f06be937b800302a0961b79ac3e7cb3f16
Franco Aquistapace <francoaquistapace@Francos-MacBook-Pro.local>
2023-06-20 16:12:25 -0300

    Defined SC lattice generator in tools.

--- tools.py
+++ tools.py
@@ -13,4 +13,40 @@
 #   limitations under the License.
 
 # ----- Here we'll define some tools to help us in development and testing ------
+# We'll use a functional programming approach for now, we can develop classes 
+# later if we need them.
+
+# Import modules
 import numpy as np
+
+# Function that can generate a cubic structure
+def gen_sc_lattice(n1, n2, n3, a):
+    '''
+    Params:
+        n1, n2, n3 : int
+            Size of the lattice in the x, y and z
+            directions, respectively, in terms of
+            lattice unit vectors. 
+        a : float or int
+            Lattice constant in A for the lattice.
+    
+    Output:
+        Returns a numpy array of shape (N,3), where
+        N is the amount of atoms in the structure, 
+        and each row is the position of an atom as
+        (x, y, z). The array contains the positions
+        for a simple cubic (SC) lattice, with unit 
+        vectors x_hat, y_hat, z_hat, and dimensions
+        (n1*a*x_hat) x (n2*a*y_hat) x (n3*a*z_hat).
+    '''
+    config = []
+    for i in range(n1):
+        for j in range(n2):
+            for k in range(n3):
+                # Build position and add to config
+                p = [a * i, 
+                     a * j,
+                     a * k]
+                config.append(p)
+
+    return np.array(config)
