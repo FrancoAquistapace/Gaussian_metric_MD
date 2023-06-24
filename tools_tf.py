Fixed data types inside rotation function tensors.

tools_tf.py
@@ -127,19 +127,19 @@ def rotate(C, alpha, beta, gamma):
     '''
     # Define each rotation
     c_alpha, s_alpha = tf.cos(alpha), tf.sin(alpha)
-    R_x = tf.constant([[1, 0, 0],
-                       [0, c_alpha, -s_alpha],
-                       [0, s_alpha, c_alpha]], 
+    R_x = tf.constant([[1., 0., 0.],
+                       [0., c_alpha, -s_alpha],
+                       [0., s_alpha, c_alpha]], 
                        dtype='float32')
     c_beta, s_beta = tf.cos(beta), tf.sin(beta)
-    R_y = tf.constant([[c_beta, 0, s_beta],
-                       [0, 1, 0],
-                       [-s_beta, 0, c_beta]], 
+    R_y = tf.constant([[c_beta, 0., s_beta],
+                       [0., 1., 0.],
+                       [-s_beta, 0., c_beta]], 
                        dtype='float32')
     c_gamma, s_gamma = tf.cos(gamma), tf.sin(gamma)
-    R_z = tf.constant([[c_gamma, -s_gamma, 0],
-                       [s_gamma, c_gamma, 0],
-                       [0, 0, 1]],
+    R_z = tf.constant([[c_gamma, -s_gamma, 0.],
+                       [s_gamma, c_gamma, 0.],
+                       [0., 0., 1.]],
                        dtype='float32')
     # Build general rotation
     R = tf.matmul(R_z, tf.matmul(R_y, R_x))