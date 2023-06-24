dc3d0b99153643e1dd6bad2e245afa2d06c06229
Franco Aquistapace <francoaquistapace@Francos-MacBook-Pro.local>
2023-06-24 19:50:10 -0300

    Built domain generating function in TF implementation.

--- gaussian_metric_tf.py
+++ gaussian_metric_tf.py
@@ -19,3 +19,40 @@ import tensorflow as tf
 from tools_tf import *
 
 # ----- General utility functions -----------------
+
+# Define function to get cubic domain that contains
+# two configurations
+def get_cubic_domain(C1, C2, step_size, margin_size):
+    '''
+    Params:
+        C1, C2 : tf.Tensor
+            Arrays containing atomic configurations
+            C1 and C2, respectively.
+        step_size : int or float
+            Size in A of the side of a cubic volume, which
+            is used to discretise the cubic domain.
+        margin_size : int or float
+            Size in A of the margins added in each direction
+            to the cubic integration domain.
+        
+    Output:
+        Returns a cubic space that contains both C1 and C2, 
+        discretised as cubes of side step_size and represented
+        by their center vector. This space is extended in each 
+        dimension an amount 2 * margin_size. 
+    '''
+    # Build joint configuration, to help get cubic domain
+    C_joint = tf.concat([C1,C2], 0)
+    # Determine max and min values of the cubic domain
+    x_max, y_max, z_max = tf.reduce_max(C_joint, axis=0).numpy() + margin_size
+    x_min, y_min, z_min = tf.reduce_min(C_joint, axis=0).numpy() - margin_size
+    
+    # Build domain as a set of points
+    n1 = int((x_max - x_min)/step_size) + 1
+    n2 = int((y_max - y_min)/step_size) + 1
+    n3 = int((z_max - z_min)/step_size) + 1
+    dom = gen_sc_lattice(n1, n2, n3, step_size)
+    # Displace domain to its correct origin
+    dom = displace(dom, tf.constant([x_min, y_min, z_min]))
+
+    return dom
